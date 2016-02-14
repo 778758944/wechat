ea0b4f45676e11059b0431111f96d733cf5e2f0f
778758944 <778758944@qq.com>
2016-02-14 14:56:09 +0800

    modify

--- server/lib/accessToken.js
+++ server/lib/accessToken.js
@@ -127,7 +127,7 @@ var jssdk=function(model,url,Token,res){
 		}
 		else if(data[0]){
 			console.log(data);
-			var expires=data[0].expires*1000;
+			var expires=data[0].expires_in*1000;
 			var timeDiff=new Date()-data[0].time;
 			if(timeDiff<expires){
 				var data=signature(noncert,data[0].ticket,new Date().getTime(),url);
